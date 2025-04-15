bc13418fc3b3a817834263d0a335ca7c09a2303e
MrSilent2001 <bandarigodadm.21@uom.lk>
2025-04-15 14:30:32 +0530

    backend file structure created

---
+++ backend/RBAC/src/main/java/com/spring_boot_projects/RBAC/RbacApplication.java
@@ -0,0 +1,15 @@
+package com.spring_boot_projects.RBAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
